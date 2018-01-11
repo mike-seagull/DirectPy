3b5a91345fe1f341e702bd102ed56ece61325c71
Michael Hollister <hollister.michael@gmail.com>
2018-01-10 22:58:14 -0600

    converted to a library

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='directpy',
-      version='0.2',
-      description='A python library for interacting with DirecTV receivers',
-      url='http://github.com/sentry07/DirectPy',
-      author='Eric Walters',
-      license='MIT',
-      py_modules=['DirectPy'])
